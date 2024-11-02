e8086cf50a8e43ecd96598c3192d3a500904adf2
benidama <benidama02@gmail.com>
2024-11-02 07:59:19 +0000

    Create Specific Components in a React Application

---
+++ alx-react-app/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <h1>My Favorite Cities</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
