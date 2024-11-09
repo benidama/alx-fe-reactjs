704ee507e1d9f149d4313490a467037ea67e56d9
benidama <benidama02@gmail.com>
2024-11-09 20:42:11 +0000

    add small change

--- alx-react-app/src/components/Header.jsx
+++ alx-react-app/src/components/Header.jsx
@@ -5,7 +5,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <header>
+      <header
+        style={{ backgroundColor: "navy", color: "white", textAlign: "center" }}
+      >
         <h1>My Favorite Cities</h1>
       </header>
     </div>
