a1bd4ec647ce4a48b70f3f3ceccea51b6a34e268
benidama <benidama02@gmail.com>
2024-11-09 21:53:17 +0000

    Counter with useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -35,7 +36,8 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
     </>
   );
 }
