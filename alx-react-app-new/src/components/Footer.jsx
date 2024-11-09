fbe5f60ef631b573934bb8b89f8a04231b0513cf
benidama <benidama02@gmail.com>
2024-11-09 21:08:12 +0000

    Add my directory

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p style={{ fontSize: "12px", color: "cyan" }}>© 2023 City Lovers</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <div>
+      <div>
+        <main>
+          <p style={{ fontSize: "12px", color: "wheat" }}>
+            I love to visit New York, Paris, and Tokyo.
+          </p>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
