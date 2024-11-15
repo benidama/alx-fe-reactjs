1b25063f5949b8111225618215ca93fa38096120
benidama <benidama02@gmail.com>
2024-11-15 18:40:10 +0000

    Changed

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -5,10 +5,18 @@ const RecipeDetails = ({ recipeId }) => {
   const recipe = useRecipeStore((state) =>
     state.recipes.find((recipe) => recipe.id === recipeId)
   );
+  const updateRecipe = useRecipeStore((ne) =>
+    ne.recipes.find((recipe) => recipe.id === reciped)
+  );
+  const deleteRecipe = useRecipeStore((open) =>
+    open.recipes.find((recipe) => recipe.id === recipeCd)
+  );
 
   return (
     <div>
       <h1>{recipe.title}</h1>
+      <p>{updateRecipe}</p>
+      <p>{deleteRecipe}</p>
       <p>{recipe.description}</p>
       {/* Render EditRecipeForm and DeleteRecipeButton here */}
     </div>
