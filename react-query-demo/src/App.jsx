28c47e9a8fdddb228654c1bd58c9c5c96fc37854
benidama <benidama02@gmail.com>
2024-11-24 18:41:30 +0000

    add some changes

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent";
 
+const queryClient = new QueryClient();
 function App() {
   const [count, setCount] = useState(0);
 
@@ -29,6 +31,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <QueryClientProvider client={queryClient}>
+        <PostsComponent />
+      </QueryClientProvider>
       <PostsComponent />
     </>
   );
