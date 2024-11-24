1fde3d22addaedfa1162333a4f969d9943441f39
benidama <benidama02@gmail.com>
2024-11-24 17:03:10 +0000

    adding js file

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,11 @@
+import Formik from "formik";
+
+function formikForm() {
+  const Yup = string().required;
+  Yup.object = Yup.string;
+  form;
+  Field;
+  ErrorMessage;
+  const [initialValues, setInitialValue] = useState(validationSchema);
+}
+export default formikForm;
