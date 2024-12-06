750080a67331fecfa72166e7acba3f5a5536d71a
benidama <benidama02@gmail.com>
2024-12-06 20:19:22 +0000

    Fetch data

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { react } from "react-router-dom";
 
 const HomePage = () => {
-  return <div></div>;
+  const [data, setData] = useState("");
+  const newData = data.json.map((data) => {
+    data.title;
+  });
+  const result = [useParams, ingredients, instructions, image, Link, to];
+  return (
+    <div className="shadow-sm text-sm hover:before rounded-md grid grid-cols-1,grid-cols-2,grid-cols-3">
+      {newData}
+      {result}
+    </div>
+  );
 };
 
 export default HomePage;
