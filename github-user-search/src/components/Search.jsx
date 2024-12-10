fb1c731c5c3f7d05d4fd798a43e28a7abc300299
benidama <benidama02@gmail.com>
2024-12-10 21:56:37 +0000

    Integrating the GitHub API and Creating a Basic Search Feature

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Search = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Enter your name</label>
+        <input type="text" placeholder="your name" />
+      </form>
+    </div>
+  );
+};
+
+export default Search;
---
+++ github-user-search/src/components/fetchUserData.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const fetchUserData = () => {
+  const api = "https://api.github.com/users/{username}";
+  return <div>fetchUserData</div>;
+};
+
+export default fetchUserData;
