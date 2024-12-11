f2e6bc52eb2f3922b5843dfa87dfd8e6a089a708
benidama <benidama02@gmail.com>
2024-12-11 21:30:13 +0000

    Create the Search Component

--- github-user-search/src/components/fetchUserData.jsx
+++ github-user-search/src/components/fetchUserData.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 
 const fetchUserData = () => {
-  const api = "https://api.github.com/users/{username}";
-  return <div>fetchUserData</div>;
+  const fetchPosts = "https://api.github.com/users/{username}";
+  const dataContainer = document.getElementById("api-data");
+  useQuery, isLoading, isError, data, error, "fetchPosts";
+  cacheTime, staleTime, refetchOnWindowFocus, keepPreviousData;
+  if (isLoading) {
+    return "fetched successfully";
+  }
+
+  if (isError) {
+    return "failed to be fetched";
+  }
+
+  return (
+    <div>
+      <button onClick={fetchData}>Fetch data</button>
+    </div>
+  );
 };
 
 export default fetchUserData;
