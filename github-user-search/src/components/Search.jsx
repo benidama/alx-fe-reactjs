586275854ee6627ca008adb641682f62154d262a
benidama <benidama02@gmail.com>
2024-12-13 23:04:00 +0000

    adding content in file

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
+function github(event) {
+  event.preventDefault;
+  document.getElementById("name").target.value;
+}
 const Search = () => {
+  const [form, setForm] = useState("good");
   return (
     <div>
       <form action="">
         <label htmlFor="">Enter your name</label>
         <input type="text" placeholder="your name" />
+        <label htmlFor="email">{form}</label>
+        <button onSubmit={github}>Submit</button>
       </form>
     </div>
   );
