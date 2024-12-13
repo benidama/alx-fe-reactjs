cd3b8f0499f7f2c7c32d0bf1ead3b85cebb294c1
benidama <benidama02@gmail.com>
2024-12-13 23:32:15 +0000

    file changed

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -6,13 +6,17 @@ function github(event) {
 }
 const Search = () => {
   const [form, setForm] = useState("good");
+  const avatar_url = "My data";
   return (
     <div>
       <form action="">
         <label htmlFor="">Enter your name</label>
         <input type="text" placeholder="your name" />
         <label htmlFor="email">{form}</label>
-        <button onSubmit={github}>Submit</button>
+        <p>Loading</p>
+        <img src="" alt="not found" />
+        <p>Looks like we cant find the user</p>
+        <button onSubmit={github}>login</button>
       </form>
     </div>
   );
